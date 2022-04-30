62061e61010a7406c0534e04edd84370c9bf96d1
Trần Minh Hoàng (Artorias) <71833423+chnk58hoang@users.noreply.github.com>
2022-04-30 13:57:20 +0700

    cleancode

--- inference.py
+++ inference.py
@@ -15,29 +15,39 @@ WEIGHT_DECAY = 0
 EPOCHS = 50
 PIN_MEMORY = True
 
-transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(), ])
-test_dataset = VOCDataset(
-    "/kaggle/input/pascalvoc-yolo/100examples.csv", img_dir=IMG_DIR, label_dir=LABEL_DIR, transform=transform,
-)
 
-test_loader = DataLoader(
-    dataset=test_dataset,
-    batch_size=BATCH_SIZE,
-    pin_memory=PIN_MEMORY,
-    shuffle=True,
-    drop_last=True,
-)
 
-model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
 
-model.load_state_dict(torch.load('yolov1_40.pth',map_location=torch.device('cpu')))
+if __name__ == '__main__':
 
-for x, y in test_loader:
-            x = x.to(DEVICE)
-            for idx in range(8):
-                bboxes = cellboxes_to_boxes(model(x))
-                bboxes = non_max_supression(bboxes[idx], iou_threshold=0.5, prob_threshold=0.4, box_format="midpoint")
-                plot_image(x[idx].permute(1,2,0).to("cpu"), bboxes)
 
-            import sys
-            sys.exit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weight_dir',type=str)
+    args = parser.parse_args()
+
+    transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(), ])
+    test_dataset = VOCDataset(
+        "/kaggle/input/pascalvoc-yolo/100examples.csv", img_dir=IMG_DIR, label_dir=LABEL_DIR, transform=transform,
+    )
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=BATCH_SIZE,
+        pin_memory=PIN_MEMORY,
+        shuffle=True,
+        drop_last=True,
+    )
+
+    model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
+
+    model.load_state_dict(torch.load(args.weight_dir,map_location=torch.device('cpu')))
+
+    for x, y in test_loader:
+                x = x.to(DEVICE)
+                for idx in range(8):
+                    bboxes = cellboxes_to_boxes(model(x))
+                    bboxes = non_max_supression(bboxes[idx], iou_threshold=0.5, prob_threshold=0.4, box_format="midpoint")
+                    plot_image(x[idx].permute(1,2,0).to("cpu"), bboxes)
+
+                import sys
+                sys.exit()
